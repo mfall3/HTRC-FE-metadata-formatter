0b671ba8445f56861fb4947f9754eb313b242ee1
mfall3 <mfall3@illinois.edu>
2015-02-13 09:10:13 -0600

    add start/end line options and progress logging

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ HTRC-FE-metadata-formatter
 
 Reads a hathifiles text file one line at a time and creates HTRC Feature Extraction metadata json files for use in generating Feature Extraction files as described at https://sandbox.htrc.illinois.edu/HTRC-UI-Portal2/Features.
 
-Usage: python3 hathifiles2FE.py hathifile outDirectory
+Usage: python3 hathifiles2FE.py hathifile outDirectory startLine endLine
+
+A log file named hathifiles2FE_log_[timestamp].txt will be created in outDirectory.
+
+Optional arguments are only optional if no other arguments are used after them.  For example, if startLine is provided, outDirectory must be provided.
 
 ## arguments:
 
@@ -14,3 +18,11 @@ Usage: python3 hathifiles2FE.py hathifile outDirectory
 + *outDirectory* is the destination directory for the ouput metadata json files
     - optional
     - default is the current directory
+
++ *startLine* is the first line of the hathifile to be processed
+    - optional
+    - default is the first line of the hathifile
+
++ *endLine* is the last line of the hathifile to be processed
+    - optional
+    - default is the last line of the hathifile
--- hathifiles2FE.py
+++ hathifiles2FE.py
@@ -19,44 +19,96 @@ from collections import OrderedDict
 if len(sys.argv) > 1:
     hathifile = sys.argv[1]
 else:
-    print('missing name of hathifile -- Usage: python3 ' + sys.argv[0] + ' hathifile [outDirectory]')
+    print('missing name of hathifile -- Usage: python3 ' + sys.argv[0] + ' hathifile [outDirectory] [startLine] [endLine]')
     sys.exit(1)
+
+# set defaults values for optional arguments
+outDir = os.getcwd()
+startLine = 0
+
+def file_line_count(filename):
+    with open(filename) as f:
+        for i, l in enumerate(f):
+            pass
+        return i+1
+
+endLine = file_line_count(hathifile)
 
 # use the provided output directory, or default to current directory
 if len(sys.argv) > 2:
     outDir = sys.argv[2]
-else:
-    outDir = os.getcwd()
+
+def is_int(string):
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+# use provided start line, if valid
+if len(sys.argv) > 3 and is_int(sys.argv[3]) and int(sys.argv[3]) < endLine:
+    startLine = int(sys.argv[3])
+
+# use provided end line, if valid
+if len(sys.argv) > 4 and is_int(sys.argv[4]) and int(sys.argv[4]) < endLine and int(sys.argv[4]) >= startLine :
+    endLine = int(sys.argv[4])
 
 ts = time.time();
 batchtime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d'+'T'+'%H:%M')
 
-# read in one line at a time, write out one file at a time
-with open(hathifile) as f:
+linenum = 1
 
-    #rows = (line.split('t') for line in f)
+# log progress
+with open(outDir + os.sep + "hathifiles2FE_log_" + batchtime + ".txt", 'w+') as log:
 
-    for line in f:
-        row = (line.split('\t'))
-        institutionId = (row[0].split('.'))[0]
-        cleanVolumeId = row[0].replace(':', "+")
-        cleanVolumeId = cleanVolumeId.replace('/', "=")
-        if not os.path.exists(outDir + os.sep  + institutionId):
-            os.makedirs(outDir + os.sep + institutionId)
-        outfileName = outDir + os.sep + institutionId + os.sep + cleanVolumeId + '.json'
 
-        with open(outfileName, 'w') as outfile:
-            print("writing " + outfileName + " ..." )
-            meta = OrderedDict()
-            meta["schemaVersion"] = "1.2"
-            meta["dateCreated"] = batchtime
-            meta["title"] = row[11]
-            meta["pubDate"] = row[16]
-            meta["language"] = row[18]
-            meta["htBibUrl"]="http://catalog.hathitrust.org/api/volumes/full/htid/"+row[0]+ ".json"
-            meta["handleUrl"] = "http://hdl.handle.net/2027/"+row[0]
-            meta["oclc"] = row[7]
-            meta["imprint"] = row[12]
-            record = OrderedDict()
-            record["metadata"] = meta
-            json.dump(record, outfile)
+    print("startLine: " + str(startLine))
+    print("endLine: " + str(endLine))
+    log.write("startLine: " + str(startLine)  + "\n")
+    log.write("endLine: " + str(endLine)  + "\n")
+
+    # read in one line at a time, write out one file at a time, logging progress
+    with open(hathifile) as f:
+
+        for line in f:
+
+            if linenum >= startLine:
+
+                print ("reading line number " + str(linenum) + " ...")
+                log.write("reading line number " + str(linenum) + "\n")
+                row = (line.split('\t'))
+                institutionId = (row[0].split('.'))[0]
+                cleanVolumeId = row[0].replace(':', "+")
+                cleanVolumeId = cleanVolumeId.replace('/', "=")
+                if not os.path.exists(outDir + os.sep  + institutionId):
+                    os.makedirs(outDir + os.sep + institutionId)
+                outfileName = outDir + os.sep + institutionId + os.sep + cleanVolumeId + '.json'
+
+                with open(outfileName, 'w') as outfile:
+                    print("writing " + outfileName + " ..." )
+                    log.write("writing " + outfileName + "\n")
+                    meta = OrderedDict()
+                    meta["schemaVersion"] = "1.2"
+                    meta["dateCreated"] = batchtime
+                    meta["title"] = row[11]
+                    meta["pubDate"] = row[16]
+                    meta["language"] = row[18]
+                    meta["htBibUrl"]="http://catalog.hathitrust.org/api/volumes/full/htid/"+row[0]+ ".json"
+                    meta["handleUrl"] = "http://hdl.handle.net/2027/"+row[0]
+                    meta["oclc"] = row[7]
+                    meta["imprint"] = row[12]
+                    record = OrderedDict()
+                    record["metadata"] = meta
+                    json.dump(record, outfile)
+
+            linenum+=1
+            if linenum > endLine:
+                f.close()
+                log.close()
+                print("done")
+                exit()
+
+        f.close()
+    log.close()
+
+print("done")
